bcbffd4e8e2b1a9b5ca6a09a21d8c7eeb13b3888
Dwei1120 <110244356+Dwei1120@users.noreply.github.com>
2022-08-31 15:48:04 +0800

    Update BaseLazyFragment.java

--- app/src/main/java/com/github/tvbox/osc/base/BaseLazyFragment.java
+++ app/src/main/java/com/github/tvbox/osc/base/BaseLazyFragment.java
@@ -254,6 +254,13 @@ public abstract class BaseLazyFragment extends Fragment implements CustomAdapt {
             });
         }
     }
+    protected void setLoadSir2(View view) {
+        mLoadService = LoadSir.getDefault().register(view, new Callback.OnReloadListener() {
+            @Override
+            public void onReload(View v) {
+            }
+        });
+    }
 
     protected void showLoading() {
         if (mLoadService != null) {
